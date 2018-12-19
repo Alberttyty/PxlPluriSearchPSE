b437421445200e5b6bd6ff6c763393eb79cb808c
Thierry CARESMEL <36160092+Alberttyty@users.noreply.github.com>
2018-12-19 11:17:41 +0100

    Add files via upload

---
+++ PxlPluriSearchPSE.php
@@ -0,0 +1,22 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the PxlPluriSearchPSE module.                           */
+/*                                                                                   */
+/*      Copyright (c) Pixel Plurimedia                                               */
+/*      email : dev@pixel-plurimedia.fr                                              */
+/*      web : https://pixel-plurimedia.fr                                            */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace PxlPluriSearchPSE;
+
+use Thelia\Module\BaseModule;
+
+class PxlPluriSearchPSE extends BaseModule
+{
+    /** @var string */
+    const DOMAIN_NAME = 'pxlplurisearchpse';
+
+}
